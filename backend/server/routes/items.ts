5fd5aea6aff7282db79cf1ba9d7e01e6cd9cf720
Peter Fänge <pellefange@gmail.com>
2025-05-06 20:33:43 +0200

    items.ts - Changed the import route to match the new export

--- backend/server/routes/items.ts
+++ backend/server/routes/items.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import Item, { IItem } from "../models/items";
+import Item, { IItem } from "../models/item";
 
 const itemRouter = express.Router();
 
