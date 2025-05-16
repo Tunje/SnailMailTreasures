a82183ccf01e64b95a91caa5c4bafe1e89ade607
Areesha Haseeb Khan Suontausta <32227763+AreeshaHaseeb@users.noreply.github.com>
2025-05-16 02:49:46 +0200

    Updated navbar with tailwindCSS added hover work in progress

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import './index.css';
-import Navbar from './components/navbar';
+import "./index.css";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="bg-blue-500 text-white p-4 rounded">Tailwind Works!</div>
+      <div className="bg-green-500 text-white p-4 text-2xl rounded">
+        ✅ Tailwind is working now!
+      </div>
     </>
   );
 }
