08caa791cda00ba600f2f301081edf2fb0fdac61
Supanee Patsa <patsasupanee@gmail.com>
2025-05-19 10:54:01 +0200

    update

--- backend/server/routes/itemRoutes.ts
+++ backend/server/routes/itemRoutes.ts
@@ -1,39 +1,56 @@
-import express, { Request, Response } from "express";
-import Item, { ItemDocument } from "../models/itemModel";
+import express, { Request, Response } from 'express';
+import Item, { ItemDocument } from '../models/itemModel';
 
 const itemRouter = express.Router();
 
-itemRouter.get("/", async (req: Request, res: Response) => {
+itemRouter.get('/', async (req: Request, res: Response) => {
   try {
-    const items = await Item.find().populate("userId", "username email");
+    const items = await Item.find().populate('userId', 'username email');
     res.json(items);
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
     } else {
-      res.status(500).json({ message: "An unknown error occurred" });
+      res.status(500).json({ message: 'An unknown error occurred' });
     }
   }
 });
 
-itemRouter.get("/:id", async (req: Request, res: Response) => {
+itemRouter.get('/:id', async (req: Request, res: Response) => {
   try {
     const items = await Item.findById(req.params.id).populate(
-      "userId",
-      "username email"
+      'userId',
+      'username email'
     );
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
     } else {
-      res.status(500).json({ message: "An unknown error occurred" });
+      res.status(500).json({ message: 'An unknown error occurred' });
     }
   }
 });
 
-itemRouter.post("/", async (req: Request, res: Response) => {
+itemRouter.get('/search', async (req: Request, res: Response) => {
   try {
-    const { itemName, description, imageUrl, category, price, userId } = req.body;
+    const { q } = req.query;
+    const items = await Item.find({
+      itemName: { $regex: new RegExp(q as string, 'i') },
+    }).populate('userId', 'username email');
+    res.json(items);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: 'An unknown error occurred' });
+    }
+  }
+});
+
+itemRouter.post('/', async (req: Request, res: Response) => {
+  try {
+    const { itemName, description, imageUrl, category, price, userId } =
+      req.body;
     const newItem = new Item({
       itemName,
       description,
@@ -49,12 +66,12 @@ itemRouter.post("/", async (req: Request, res: Response) => {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
     } else {
-      res.status(500).json({ message: "An unknown error occurred" });
+      res.status(500).json({ message: 'An unknown error occurred' });
     }
   }
 });
 
-itemRouter.put("/:id", async (req: Request, res: Response) => {
+itemRouter.put('/:id', async (req: Request, res: Response) => {
   try {
     const updateItem = await Item.findByIdAndUpdate(
       req.params.id,
@@ -66,25 +83,29 @@ itemRouter.put("/:id", async (req: Request, res: Response) => {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
     } else {
-      res.status(500).json({ message: "An unknown error occurred" });
+      res.status(500).json({ message: 'An unknown error occurred' });
     }
   }
 });
 
-itemRouter.delete("/:id", async (req: Request, res: Response): Promise<any> => {
+itemRouter.delete('/:id', async (req: Request, res: Response): Promise<any> => {
   try {
     const deletedItem = await Item.findByIdAndDelete(req.params.id);
 
     if (!deletedItem) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: 'User not found' });
     }
 
-    res.status(200).json({ message: `Item by name '${deletedItem.itemName}' with ID (ID: ${deletedItem._id}) was successfully deleted.`});
+    res
+      .status(200)
+      .json({
+        message: `Item by name '${deletedItem.itemName}' with ID (ID: ${deletedItem._id}) was successfully deleted.`,
+      });
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
     } else {
-      res.status(500).json({ message: "An unknown error occurred" });
+      res.status(500).json({ message: 'An unknown error occurred' });
     }
   }
 });
--- backend/server/server.ts
+++ backend/server/server.ts
@@ -1,34 +1,36 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import userRouter from "./routes/users";
-import itemRouter from "./routes/items";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import userRouter from './routes/userRoutes';
+import itemRouter from './routes/itemRoutes';
+
 
 const app = express();
 const PORT = 3000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000" })); // Allow requests from the React frontend
+app.use(cors({ origin: 'http://localhost:5173' })); // Allow requests from the React frontend
 app.use(express.json());
 
 // MongoDB connection
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/snailmail");
-    console.log("MongoDB connected successfully");
+    await mongoose.connect('mongodb://localhost:27017/snailmail');
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error('Error connecting to MongoDB:', error);
     process.exit(1);
   }
 };
 
-mongoose.connection.on("error", (err) =>
+mongoose.connection.on('error', (err) =>
   console.error(`MongoDB connection error: ${err}`)
 );
 
 // Routes
-app.use("/api/users", userRouter); // All routes begin with /api/users
-app.use("/api/items", itemRouter); // All routes begin with /api/items
+app.use('/api/users', userRouter); // All routes begin with /api/users
+app.use('/api/items', itemRouter); // All routes begin with /api/items
+
 
 // Start server
 connectToDatabase();
