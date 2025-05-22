9c4bdcc7995e521a6be5b4e5d5bc251b1d56eeca
Peter Fänge <pellefange@gmail.com>
2025-05-22 17:08:55 +0200

    PeterF - moved swagger JSON comments to userRoutes.ts

--- backend/server/routes/userRoutes.ts
+++ backend/server/routes/userRoutes.ts
@@ -9,6 +9,42 @@ import {
 
 const userRouter = express.Router();
 
+/**
+ * @swagger
+ * /users:
+ *    get:
+ *      summary: Get all users
+ *      responses:
+ *         200:
+ *           description: A list of all users
+ *           content: 
+ *             application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                 $ref: '#/components/schemas/User'
+ */
+
+/**
+ * @swagger
+ * /users/{id}:
+ *    get:
+ *      summary: Get a user by ID
+ *      tags: [User]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          schema: 
+ *           type: integer
+ *      responses:
+ *         200:
+ *           description: A user object
+ *           content: 
+ *             application/json:
+ *              schema:
+ *                 $ref: '#/components/schemas/User'
+ */
 userRouter.get("/allusers", getAllUsers);
 userRouter.get("/user/:id", getUserById);
 userRouter.get("/user/:userName", getUserByUsername);
