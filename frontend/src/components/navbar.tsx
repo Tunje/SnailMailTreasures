28be30dbae79bd3747e5c3e46ff398495087ba7b
Areesha Haseeb Khan Suontausta <32227763+AreeshaHaseeb@users.noreply.github.com>
2025-05-24 06:41:33 +0200

    made the nav smaller and turned circles to capsules

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -7,14 +7,14 @@ export default function Navbar() {
   const [cartCount, setCartCount] = useState(0);
 
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-[#FDF4DF] pt-2 pb-4">
+    <nav className="fixed top-0 left-0 w-full z-50 bg-[#FDF4DF] pt-1 pb-2">
       <div className="px-4 flex flex-col gap-4 md:flex-row md:justify-between md:items-center">
         {/* Logo */}
         <div className="flex items-center gap-6">
           <img
             src="/snailmail-logo.png"
             alt="Snailmail Treasures Logo"
-            className="w-[102px] h-[100px] cursor-pointer hover:scale-105 transition"
+            className="w-[85px] h-[75px] cursor-pointer hover:scale-105 transition"
             onClick={() => {
               console.log("Logo clicked");
               navigate("/");
@@ -23,7 +23,7 @@ export default function Navbar() {
         </div>
 
         {/* Search */}
-        <div className="flex items-center bg-white px-4 py-2 rounded-full shadow-md w-full md:w-[500px]">
+        <div className="flex items-center bg-white px-2 py-1 rounded-half shadow-md w-full md:w-[600px]">
           <input
             type="text"
             placeholder="Search..."
@@ -31,11 +31,11 @@ export default function Navbar() {
           />
           <button
             className={`px-4 py-1 rounded-lg text-sm transition
-    ${
-      activeNav === "search"
-        ? "bg-blue-400 text-white scale-105"
-        : "bg-[#7A7C56] text-white hover:scale-105 active:scale-95"
-    }`}
+          ${
+            activeNav === "search"
+              ? "bg-blue-400 text-white scale-105"
+              : "bg-[#7A7C56] text-white hover:scale-105 active:scale-95"
+          }`}
             onClick={() => {
               setActiveNav("search");
               setTimeout(() => setActiveNav(null), 500);
@@ -48,7 +48,7 @@ export default function Navbar() {
         {/* Login/Cart */}
         <div className="flex gap-4 flex-wrap justify-center md:justify-end">
           <button
-            className={`rounded-full font-grover px-4 py-3 text-lg transition
+            className={`rounded-full font-grover px-3 py-0 text-lg transition
             ${
               activeNav === "login"
                 ? "bg-blue-400 text-white scale-105"
@@ -65,7 +65,7 @@ export default function Navbar() {
 
           <div className="relative">
             <button
-              className={`rounded-full font-grover px-4 py-3 text-lg transition
+              className={`rounded-full font-grover px-4 py-1 text-lg transition
             ${
               activeNav === "cart"
                 ? "bg-blue-400 text-white scale-105"
@@ -88,22 +88,22 @@ export default function Navbar() {
       </div>
 
       {/* Banner */}
-      <div className="clip-banner bg-[#CB8427] text-center py-6 md:py-8 mt-4 mb-2 md:mt-2 md:mb-1 w-full">
-        <h1 className="text-5xl font-grover">Snail Mail</h1>
+      <div className="clip-banner bg-[#CB8427] text-center py-4 md:py-4 mt-2 mb-1 md:mt-1 md:mb-2 w-full">
+        <h1 className="text-4xl font-grover">Snail Mail</h1>
       </div>
 
       {/* Circles */}
-      <div className="flex justify-around mt-[-4px] mb-8">
-        {["shop", "home", "deals", "favorites"].map((label) => (
+      <div className="flex justify-around mt-[-0px] mb-0">
+        {["shop", "home", "deals", "profiles"].map((label) => (
           <div
             key={label}
-            className={`w-[80px] h-[75px] flex items-center justify-center rounded-full shadow-md font-grover text-lg font-bold
-                cursor-pointer transition
-                ${
-                  activeNav === label
-                    ? "bg-blue-400  text-white"
-                    : "bg-[#D89C6A] hover:scale-105 active:scale-95"
-                }`}
+            className={`w-[90px] h-[55px] flex items-center justify-center rounded-full shadow-md font-grover text-lg font-bold
+            cursor-pointer transition
+            ${
+              activeNav === label
+                ? "bg-blue-400  text-white"
+                : "bg-[#D89C6A] hover:scale-105 active:scale-95"
+            }`}
             onClick={() => {
               setActiveNav(label);
               navigate(`/${label}`);
