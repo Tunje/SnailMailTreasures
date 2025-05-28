4fe490348cd940f4d5c1b04a562a9164c1367463
Peter Fänge <pellefange@gmail.com>
2025-05-28 22:05:56 +0200

    PeterF - updated routes to include authMiddleware for those routes that require authenticated users

--- backend/server/routes/itemRoutes.ts
+++ backend/server/routes/itemRoutes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { protect } from "../middleware/authMiddleware";
 import { 
   getAllItems, 
   getItemById, 
@@ -11,8 +12,8 @@ const itemRouter = express.Router();
 
 itemRouter.get("/allitems", getAllItems);
 itemRouter.get("/item/:id", getItemById);
-itemRouter.post("/createitem", createItem);
-itemRouter.put("/updateitem/:id", updateItemById);
-itemRouter.delete("/deleteitem/:id", deleteItem);
+itemRouter.post("/createitem", protect, createItem);
+itemRouter.put("/updateitem/:id", protect, updateItemById);
+itemRouter.delete("/deleteitem/:id", protect, deleteItem);
 
 export default itemRouter;
