32dcc9de86aa345e213c84d988dc95c03019ed11
Peter Fänge <pellefange@gmail.com>
2025-05-28 21:24:47 +0200

    PeterF - adjusted code to handle input from the .env file

--- backend/server/utils/generateToken.ts
+++ backend/server/utils/generateToken.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 // Generate a JWT (JSON Web Token) for a given user ID
 export const generateToken = (userId: string): string => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET as string, {
-    expiresIn: "1d", // Token set to expire in 1 day
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN || "1d", // Token set to expire in 1 day
   });
 };
