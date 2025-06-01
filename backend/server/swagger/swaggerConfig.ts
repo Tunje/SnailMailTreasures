91cd395b210c9e114be8e66364d3de1c7f17cfbf
Peter Fänge <pellefange@gmail.com>
2025-06-01 17:23:12 +0200

    PeterF - renamed swagger.ts to swaggerConfig.ts

--- backend/server/swagger/swaggerConfig.ts
+++ backend/server/swagger/swaggerConfig.ts
@@ -102,8 +102,4 @@ const options = {
 };
 
 const swaggerSpec = swaggerJSDoc(options);
-
-export const setupSwagger = (app: Express) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log("Swagger docs available at http://localhost:3000/api-docs");
-};
+export default swaggerSpec;
