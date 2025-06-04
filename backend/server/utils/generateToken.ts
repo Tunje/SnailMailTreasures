10f8f21e88488f58e1c72bccf55e6c961db0a79e
Peter Fänge <pellefange@gmail.com>
2025-06-04 14:49:43 +0200

    PeterF - Error persists, checkout to other branch

--- backend/server/utils/generateToken.ts
+++ backend/server/utils/generateToken.ts
@@ -3,9 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET environment variable is not defined");
+}
+
 // Generate a JWT (JSON Web Token) for a given user ID
 export const generateToken = (userId: string): string => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+  return jwt.sign({ id: userId }, JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN || "1d", // Token set to expire in 1 day
   });
 };
